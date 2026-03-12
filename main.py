fd20f76dcb45e9682206e71c35181fe2305e023f
Vulcan <logmedha09@gmail.com>
2026-03-12 13:52:03 +0000

    feat: added a list challenge function

--- main.py
+++ main.py
@@ -1,22 +1,28 @@
 import typer
 import subprocess
+from rich import print
 
 app = typer.Typer()
 
+challenges = {"python": {"example-python": "https://github.com/Recticode/example-python"}}
+challenge_names = {"example-python": challenges['python']['example-python']}
+
 @app.command()
 def start(challenge_name):
-    challenges = {"example-python": "https://github.com/Recticode/example-python"}
-    if challenge_name in challenges:
-        cmd = ["git", "clone", challenges[challenge_name]]
-
+    if challenge_name in challenge_names:
+        cmd = ["git", "clone", challenge_names[challenge_name]]
         subprocess.run(cmd, check=True)
         print("cloned repo")
     else:
         print("not a challenge name")
 
 @app.command()
-def hi():
-    print("hi!")
+def list_challenges():
+    print("[bold][yellow]All Challenges[/yellow][/bold]")
+    for language in challenges:
+        print(f"[bold][green]{language.title()}[/bold][/green]")
+        for challenge in challenges[language]:
+            print(challenge)
 
 if __name__ == "__main__":
     app()
